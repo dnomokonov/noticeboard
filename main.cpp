031daaa24d52cc319f8c0f4f58d22b9740ba2b84
dnomokonov <zmnomokon715@gmail.com>
2024-10-23 07:55:52 +0300

    Set auth window (dev version)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
+#include "authwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    // MainWindow w;
+    AuthWindow w;
+
     w.show();
     return a.exec();
 }
