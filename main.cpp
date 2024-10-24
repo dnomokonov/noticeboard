5ee2d41b93e29218d88a8b20a3d12aa19342634a
dnomokonov <zmnomokon715@gmail.com>
2024-10-24 09:44:47 +0300

    Add style's

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include "authwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file(":/styles/style.css");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
+
     // MainWindow w;
     AuthWindow w;
-
     w.show();
     return a.exec();
 }
