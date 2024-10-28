a5d854e6df02c257bc6731aa3db6bd2ee0ac6b4b
dnomokonov <zmnomokon715@gmail.com>
2024-10-28 20:58:36 +0700

    Positioning of elements - finished

--- authwindow.cpp
+++ authwindow.cpp
@@ -30,7 +30,6 @@ AuthWindow::AuthWindow(QWidget *parent)
     gridLayout->setContentsMargins(0, 0, 0, 0);
     gridLayout->setSpacing(0);
 
-    // Header
     QWidget *header = new QWidget(this);
     QHBoxLayout *headerLayout = new QHBoxLayout(header);
     header->setFixedHeight(80);
@@ -48,36 +47,144 @@ AuthWindow::AuthWindow(QWidget *parent)
     headerLayout->addWidget(logoLabel, 0, Qt::AlignLeft);
     gridLayout->addWidget(header, 0, 0, 1, 2);
 
-    // Body
     QWidget *body = new QWidget(this);
-    QHBoxLayout *bodyLayout = new QHBoxLayout(body);
+    QVBoxLayout *bodyLayout = new QVBoxLayout(body);
+    bodyLayout->setAlignment(Qt::AlignCenter);
 
     QStackedWidget *stackedWidget = new QStackedWidget(this);
-    QHBoxLayout *stackedLayout = new QHBoxLayout(stackedWidget);
     stackedWidget->setObjectName("blockFormAuth");
-    stackedWidget->setFixedSize(540, 690);
+    stackedWidget->setFixedSize(540, 580);
 
     QVBoxLayout *centerLayout = new QVBoxLayout();
-    centerLayout->addWidget(stackedWidget, Qt::AlignCenter);
+    centerLayout->setAlignment(Qt::AlignCenter);
+    centerLayout->addWidget(stackedWidget);
     bodyLayout->addLayout(centerLayout);
 
-    QWidget *loginPage = new QWidget(stackedWidget);
-    QHBoxLayout *loginLayout = new QHBoxLayout(loginPage);
-    loginLayout->addWidget(loginPage, Qt::AlignCenter);
-    stackedLayout->addLayout(loginLayout);
+    // <--- Login --->
 
-    QLineEdit *usernameField = new QLineEdit(loginPage);
-    QLineEdit *passwordField = new QLineEdit(loginPage);
-    usernameField->setPlaceholderText("Username");
-    passwordField->setPlaceholderText("Password");
-    loginLayout->addWidget(usernameField);
-    loginLayout->addWidget(passwordField);
+    QWidget *loginPage = new QWidget();
+    QVBoxLayout *loginLayout = new QVBoxLayout(loginPage);
+    loginLayout->setAlignment(Qt::AlignCenter);
+
+    QLabel *headerLoginName = new QLabel("Login");
+    headerLoginName->setObjectName("headerName");
+
+    QLineEdit *loginUsernameField = new QLineEdit(loginPage);
+    QLineEdit *loginPasswordField = new QLineEdit(loginPage);
+
+    loginUsernameField->setPlaceholderText("Username");
+    loginPasswordField->setPlaceholderText("Password");
+
+    loginUsernameField->setObjectName("defaultField");
+    loginPasswordField->setObjectName("defaultField");
+
+    QHBoxLayout *headerFormLayout = new QHBoxLayout();
+    headerFormLayout->addStretch();
+    headerFormLayout->addWidget(headerLoginName);
+    headerFormLayout->addStretch();
 
     QPushButton *loginBtn = new QPushButton("Login", loginPage);
-    loginLayout->addWidget(loginBtn);
+    loginBtn->setObjectName("authBtn");
+    loginBtn->setFixedSize(250, 30);
 
+    QPushButton *switchToRegisterBtn = new QPushButton("Sign Up", loginPage);
+    switchToRegisterBtn->setObjectName("authBtn");
+    switchToRegisterBtn->setFixedSize(250, 30);
+
+    QHBoxLayout *loginBtnLayout = new QHBoxLayout();
+    loginBtnLayout->addStretch();
+    loginBtnLayout->addWidget(loginBtn);
+    loginBtnLayout->addStretch();
+    QHBoxLayout *registerBtnLayout = new QHBoxLayout();
+    registerBtnLayout->addStretch();
+    registerBtnLayout->addWidget(switchToRegisterBtn);
+    registerBtnLayout->addStretch();
+
+    loginLayout->addLayout(headerFormLayout);
+    loginLayout->addWidget(loginUsernameField);
+    loginLayout->addWidget(loginPasswordField);
+    loginLayout->addLayout(loginBtnLayout);
+    loginLayout->addLayout(registerBtnLayout);
+
+    stackedWidget->addWidget(loginPage);
+
+    // <--- Registration --->
+
+    QWidget *registerPage = new QWidget(stackedWidget);
+    QVBoxLayout *registerLayout = new QVBoxLayout(registerPage);
+    registerLayout->setAlignment(Qt::AlignCenter);
+
+    QLabel *headerRegisterName = new QLabel("Register");
+    headerRegisterName->setObjectName("headerName");
+
+    QLineEdit *registerNameField = new QLineEdit(registerPage);
+    QLineEdit *registerSurnameField = new QLineEdit(registerPage);
+    QLineEdit *registerMiddleNameField = new QLineEdit(registerPage);
+    QLineEdit *registerUsernameField = new QLineEdit(registerPage);
+    QLineEdit *registerPasswordField = new QLineEdit(registerPage);
+    QLineEdit *confirmPasswordField = new QLineEdit(registerPage);
+
+    registerNameField->setPlaceholderText("Name");
+    registerSurnameField->setPlaceholderText("Surname");
+    registerMiddleNameField->setPlaceholderText("MiddleName");
+    registerUsernameField->setPlaceholderText("Username");
+    registerPasswordField->setPlaceholderText("Password");
+    confirmPasswordField->setPlaceholderText("Confirm Password");
+    registerPasswordField->setEchoMode(QLineEdit::Password);
+    confirmPasswordField->setEchoMode(QLineEdit::Password);
+
+    registerNameField->setObjectName("defaultField");
+    registerSurnameField->setObjectName("defaultField");
+    registerMiddleNameField->setObjectName("defaultField");
+    registerUsernameField->setObjectName("defaultField");
+    registerPasswordField->setObjectName("defaultField");
+    confirmPasswordField->setObjectName("defaultField");
+
+    QHBoxLayout *headerFormRegLayout = new QHBoxLayout();
+    headerFormRegLayout->addStretch();
+    headerFormRegLayout->addWidget(headerRegisterName);
+    headerFormRegLayout->addStretch();
+
+    QPushButton *registerBtn = new QPushButton("Register", registerPage);
+    registerBtn->setFixedSize(250, 30);
+
+    QPushButton *switchToLoginBtn = new QPushButton("Back to Login", registerPage);
+    switchToLoginBtn->setFixedSize(250, 30);
+
+    QHBoxLayout *regbtnLayout = new QHBoxLayout();
+    QHBoxLayout *logbtnLayout = new QHBoxLayout();
+
+    regbtnLayout->addStretch();
+    regbtnLayout->addWidget(registerBtn);
+    regbtnLayout->addStretch();
+
+    logbtnLayout->addStretch();
+    logbtnLayout->addWidget(switchToLoginBtn);
+    logbtnLayout->addStretch();
+
+    registerLayout->addLayout(headerFormRegLayout);
+    registerLayout->addWidget(registerNameField);
+    registerLayout->addWidget(registerSurnameField);
+    registerLayout->addWidget(registerMiddleNameField);
+    registerLayout->addWidget(registerUsernameField);
+    registerLayout->addWidget(registerPasswordField);
+    registerLayout->addWidget(confirmPasswordField);
+    registerLayout->addLayout(regbtnLayout);
+    registerLayout->addLayout(logbtnLayout);
+
+    stackedWidget->addWidget(registerPage);
     gridLayout->addWidget(body, 1, 0);
     gridLayout->setRowStretch(1, 1);
+
+    // <--- Connects --->
+
+    connect(switchToRegisterBtn, &QPushButton::clicked, [=]() {
+        stackedWidget->setCurrentWidget(registerPage);
+    });
+
+    connect(switchToLoginBtn, &QPushButton::clicked, [=]() {
+        stackedWidget->setCurrentWidget(loginPage);
+    });
 }
 
 AuthWindow::~AuthWindow()
