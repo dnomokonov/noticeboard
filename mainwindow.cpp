abc0bb5379b2d5325e7e3ba4a10f493d38d7b3fb
dnomokonov <zmnomokon715@gmail.com>
2024-11-02 12:59:15 +0300

    markup header - completed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,6 +32,7 @@ MainWindow::MainWindow(QWidget *parent)
     QWidget *header = new QWidget(this);
     QHBoxLayout *headerLayout = new QHBoxLayout(header);
     header->setFixedHeight(80);
+    header->setObjectName("header");
 
     QLabel *logoLabel = new QLabel(this);
     QPixmap logoPixmap(":/biz/logo");
@@ -42,35 +43,81 @@ MainWindow::MainWindow(QWidget *parent)
     header->setContentsMargins(50, 0, 0, 0);
     headerLayout->setContentsMargins(0, 0, 0, 0);
     headerLayout->setSpacing(0);
-    header->setStyleSheet("background-color: #F5F5F5;");
     headerLayout->addWidget(logoLabel, 0, Qt::AlignLeft);
 
     QWidget *controlPanel = new QWidget(header);
     QHBoxLayout *controlPanelLayout = new QHBoxLayout(controlPanel);
 
     // category, search, geo
-    QLabel *searchIconLabel = new QLabel(controlPanel);
-    searchIconLabel->setPixmap(QPixmap(":/icons/search"));
-    searchIconLabel->setFixedSize(20, 20);
-    searchIconLabel->setScaledContents(true);
+    QPixmap iconMenu(":/icons/burgermenu");
+    QIcon categoryButtonIcon(iconMenu);
+
+    QPushButton *categoryButton = new QPushButton(controlPanel);
+    categoryButton->setObjectName("customIconBtn");
+    categoryButton->setFixedSize(30, 30);
+    categoryButton->setIcon(iconMenu);
 
     QLineEdit *searchInput = new QLineEdit(controlPanel);
-    searchInput->setFixedHeight(30);
+    searchInput->setObjectName("searchInput");
     searchInput->setPlaceholderText("Search...");
-    searchInput->setStyleSheet("border: 2px solid #8000FF; border-radius: 15px; padding-left: 10px;");
 
     QPushButton *searchButton = new QPushButton("Найти", controlPanel);
+    searchButton->setObjectName("customBtn");
     searchButton->setFixedSize(80, 30);
-    searchButton->setStyleSheet("background-color: #8000FF; color: white; border-radius: 15px;");
+    searchButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
-    controlPanelLayout->addWidget(searchIconLabel);
+    QLabel *geoBtnLabel = new QLabel(controlPanel);
+    geoBtnLabel->setPixmap(QPixmap(":/icons/geo"));
+    geoBtnLabel->setFixedSize(18, 22);
+    geoBtnLabel->setScaledContents(true);
+
+    controlPanelLayout->addWidget(categoryButton);
     controlPanelLayout->addWidget(searchInput);
     controlPanelLayout->addWidget(searchButton);
+    controlPanelLayout->addWidget(geoBtnLabel);
 
-    headerLayout->addWidget(controlPanel, 1, Qt::AlignCenter);
+    // btn add notice
+    QWidget *btnAdd = new QWidget(header);
+    QHBoxLayout *btnAddLayout = new QHBoxLayout(btnAdd);
+
+    QPushButton *btnAddNotice = new QPushButton("Разместить объявление", btnAdd);
+    btnAddNotice->setFixedSize(200, 30);
+    btnAddNotice->setObjectName("customBtn");
+
+    btnAddLayout->addWidget(btnAddNotice);
 
     //bagshop, favourite, profile
+    QWidget *userPanel = new QWidget(header);
+    QHBoxLayout *userPanelLayout = new QHBoxLayout(userPanel);
+    userPanel->setContentsMargins(0, 0, 50, 0);
 
+    QLabel *favBtnLabel = new QLabel(userPanel);
+    QLabel *shopbagBtnLabel = new QLabel(userPanel);
+    QLabel *profileBtnnLabel = new QLabel(userPanel);
+
+    favBtnLabel->setPixmap(QPixmap(":/icons/likes"));
+    shopbagBtnLabel->setPixmap(QPixmap(":/icons/shopbag"));
+    profileBtnnLabel->setPixmap(QPixmap(":/icons/profile"));
+
+    favBtnLabel->setFixedSize(24, 22);
+    shopbagBtnLabel->setFixedSize(22, 22);
+    profileBtnnLabel->setFixedSize(22, 22);
+    favBtnLabel->setScaledContents(true);
+    shopbagBtnLabel->setScaledContents(true);
+    profileBtnnLabel->setScaledContents(true);
+
+    userPanelLayout->addWidget(favBtnLabel);
+    userPanelLayout->addSpacing(16);
+    userPanelLayout->addWidget(shopbagBtnLabel);
+    userPanelLayout->addSpacing(16);
+    userPanelLayout->addWidget(profileBtnnLabel);
+
+    headerLayout->addSpacing(50);
+    headerLayout->addWidget(controlPanel, 1, Qt::AlignCenter);
+    headerLayout->addStretch();
+    headerLayout->addWidget(btnAdd, 0, Qt::AlignRight);
+    headerLayout->addStretch();
+    headerLayout->addWidget(userPanel, 0, Qt::AlignRight);
 
     // <--- Body --->
 
