9c0e854ddf4ae92e4069b1cc4c26e32a0bbae6f0
dnomokonov <zmnomokon715@gmail.com>
2024-11-13 08:37:43 +0300

    New widget

---
+++ listadverts.h
@@ -0,0 +1,17 @@
+#ifndef LISTADVERTS_H
+#define LISTADVERTS_H
+
+#include <QWidget>
+#include <QFile>
+#include <QVBoxLayout>
+#include <QLabel>
+
+class listAdverts : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit listAdverts(QWidget *parent = NULL);
+};
+
+#endif // LISTADVERTS_H
