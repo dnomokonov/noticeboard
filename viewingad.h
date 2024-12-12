d421dadd2f88b6742c7697f2d9f44c3f41106e9f
dnomokonov <zmnomokon715@gmail.com>
2024-12-12 07:45:56 +0300

    Update logical

--- viewingad.h
+++ viewingad.h
@@ -5,13 +5,39 @@
 #include <QFile>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QPainter>
+#include <QPainterPath>
+
+#include "mainwindow.h"
 
 class viewingad : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit viewingad(QWidget *parent = NULL);
+    explicit viewingad(MainWindow *mainWindow, QWidget *parent = NULL);
+    void loadAdData(int announcementID);
+
+private:
+    MainWindow *mainWindow;
+    KeeperData* keeper = mainWindow->getKeeperData();
+
+    QLabel *titleLabel;
+    QLabel *descriptionLabel;
+    QLabel *costLabel;
+    QLabel *phoneLabel;
+    QLabel *categoryLabel;
+    QLabel *locationLabel;
+    QLabel *photoLabel;
+    QWidget *sellerWidget;
+    QGridLayout *sellerLayout;
+    QLabel *sellerHeader;
+    QGridLayout *sellerBlock;
+    QLabel *sellerFullName;
+    QGridLayout *starsGridLayout;
+
+    QPixmap createRoundedPixmap(const QPixmap &src, int radius);
+
 };
 
 #endif // VIEWINGAD_H
